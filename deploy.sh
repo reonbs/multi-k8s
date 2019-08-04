38b827a63eb95ce17723ae9017623e77e727a814
Ekene <raphkens@live.com>
2019-08-04 10:58:21 +0100

    added deployment scrupt

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+docker build -t reonbs/multi-client:latest -t reonbs/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t reonbs/multi-server:latest -t reonbs/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t reonbs/multi-worker:latest -t reonbs/multi-client:$SHA -f ./worker/Dockerfile ./worker
+docker push reonbs/multi-client:latest
+docker push reonbs/multi-server:latest
+docker push reonbs/multi-worker:latest
+
+docker push reonbs/multi-client:$SHA
+docker push reonbs/multi-server:$SHA
+docker push reonbs/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=reonbs/multi-server:$SHA
+kubectl set image deployments/client-deployment server=reonbs/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=reonbs/multi-worker:$SHA
+
+
+
+
